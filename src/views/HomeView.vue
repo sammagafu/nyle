0e8f49efaaf6b91256138ed9dfadf4eda7b9df8d
Samuel Magafu <hi@sammagafu.com>
2023-12-30 01:59:19 +0300

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+  <div class="py-32">
+    <TheWelcome />
+  </div>
+  </main>
+</template>
